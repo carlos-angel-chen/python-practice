94c35e7b7fea47344eb86f6c5628708849f82763
Carlos <31170466+CarlosAngelChen@users.noreply.github.com>
2022-10-08 16:26:16 -0300

    ,

--- 5_roman2decimal.py
+++ 5_roman2decimal.py
@@ -1,3 +1,6 @@
+from turtle import right
+
+
 romanNumbers = {'I':1,
                 'V':5,
                 'X':10,
@@ -6,5 +9,18 @@ romanNumbers = {'I':1,
                 'D':500,
                 'M':1000}
 
-def roman2decimal(romanNum):
-    pass
+def roman2decimal(num):
+    sum = 0
+    for i in range(len(num)-1):
+        left = num[i]
+        right = num[i+1]
+        if romanNumbers[right]>romanNumbers[left]:
+            sum -= romanNumbers[left]
+        else: 
+            sum += romanNumbers[right]
+    sum += romanNumbers[num[-1]]
+    return sum
+
+roman = "XXL"
+result = roman2decimal(roman)
+print(result)
