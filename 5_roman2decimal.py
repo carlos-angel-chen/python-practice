8075db4fcb9419b0504207f5bf7ca93498bb0b8a
Carlos <31170466+CarlosAngelChen@users.noreply.github.com>
2022-10-08 02:24:30 -0300

    5

---
+++ 5_roman2decimal.py
@@ -0,0 +1,10 @@
+romanNumbers = {'I':1,
+                'V':5,
+                'X':10,
+                'L':50,
+                'C':100,
+                'D':500,
+                'M':1000}
+
+def roman2decimal(romanNum):
+    pass
